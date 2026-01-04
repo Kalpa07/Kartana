d44b7f5ae4214752a2ea05064cc0ea7d168c95a8
Kalpa07 <kalpateli0711@gmail.com>
2026-01-04 18:17:02 +0530

    fixed bug

--- kartana-frontend/src/lib/apollo.js
+++ kartana-frontend/src/lib/apollo.js
@@ -1,7 +1,8 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: process.env.NEXT_PUBLIC_GRAPHQL_URL || "http://localhost:4000/graphql",
+  uri: "/api/graphql",
+  // uri: process.env.NEXT_PUBLIC_GRAPHQL_URL || "http://localhost:4000/graphql",
   cache: new InMemoryCache(),
   credentials: "include",
 });
